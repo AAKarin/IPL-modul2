74e05a65f67c77565006f56970a56e1f4e438379
MYN2997 <hawkinsdamien007@gmail.com>
2025-10-23 13:54:37 +0700

    berhasil/selesai membuat kode menggunaka metode OCP

---
+++ Modul2/LatihanOCPrinciple/src/Sesudah/Cinema.java
@@ -0,0 +1,6 @@
+package Sesudah;
+
+abstract class Cinema {
+    public Double price;
+    abstract Double calculateAdminFee();
+}
---
+++ Modul2/LatihanOCPrinciple/src/Sesudah/StandardCinema.java
@@ -0,0 +1,11 @@
+package Sesudah;
+
+public class StandardCinema extends Cinema {
+    public StandardCinema(Double price) {
+        this.price = price;
+    }
+    @Override
+    Double calculateAdminFee() {
+        return price * 10 / 100;
+    }
+}
